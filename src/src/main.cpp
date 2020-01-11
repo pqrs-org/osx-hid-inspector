051379ac9995b4d0df6b1bbf76c05f8224302824
Takayama Fumihiko <tekezo@pqrs.org>
2020-01-12 00:28:53 +0900

    add version

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -3,18 +3,26 @@
 void output_all_properties_json(void);
 void output_usages(void);
 
+namespace {
+const char* version = "0.0.0";
+}
+
 int main(int argc, char* argv[]) {
-  cxxopts::Options options("hid-inspector", "List hid devices");
+  cxxopts::Options options("hid-inspector", "Show details of human input devices.");
   options.add_options()("help", "Print help.");
 
   options.add_options("Output format")                                        //
+      ("version", "Output version.")                                          //
       ("output-all-properties-json", "Output all properties in json format.") //
       ("output-usages", "Output usage pages and usages.");                    //
 
   try {
     auto parse_result = options.parse(argc, argv);
 
-    if (parse_result.count("output-all-properties-json")) {
+    if (parse_result.count("version")) {
+      std::cout << "version " << version << std::endl;
+
+    } else if (parse_result.count("output-all-properties-json")) {
       output_all_properties_json();
 
     } else if (parse_result.count("output-usages")) {
@@ -23,6 +31,7 @@ int main(int argc, char* argv[]) {
     } else {
       std::cout << options.help() << std::endl;
       std::cout << "Examples:" << std::endl;
+      std::cout << "  hid-inspector --version" << std::endl;
       std::cout << "  hid-inspector --output-all-properties-json" << std::endl;
       std::cout << "  hid-inspector --output-usages" << std::endl;
       std::cout << std::endl;
